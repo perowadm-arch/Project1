3044e9868f893e0d3ab8a5f72609006b9b4fff24
dasha <perowa.d.m@gmail.com>
2025-09-12 19:32:50 +0300

    shedevroconspect

--- Project1/ffff.cpp
+++ Project1/ffff.cpp
@@ -2,6 +2,8 @@
 #include "example.h"
 //2222222222
 
+extern int x;
+
 int main() {
 
 	example example;
@@ -9,5 +11,56 @@ int main() {
 	f();
 
 	std::cout << "Hello world" << std::endl;
+
+	//ідентифікатор - ім'я. не може починатись з цифри.
+
+	int /*min*/; //ctrl+c - comment виділеного слова
+	int Min;
+	int _min;
+	//int 2k;
+
+	5 + 7;
+
+
+	// [класс пам'яті] [const] тип ім'я [ініціалізація];
+	//в дужках не обов'язкове
+
+	int x = 5;
+	float y = 2.5;  //дійсна змінна
+	const double pi = 3.141592653589793;  //константа, має більшу пам'ять
+
+	std::cout << x << std::endl;
+
+	//auto - автоматично підібрати тип, якщо це можливо.
+	//extern - змінна створена в іншому файлі.
+
+	std::cout << ::x << std::endl;
+	::x = 90;
+	std::cout << ::x << std::endl;
+
+	// якщо підкреслити х як ::х, то ми звертаємось до глобальної змінної
+
+	// static створюється один раз на весь блок,
+	//коли ж int - кожен раз створюється наново.
+	
+	
+	// LNK - помилка лінковки. C - помилка компіляції.
+
+	void f(); {
+		int a = 10;
+		++a; //збільшує на один
+		std::cout << a << std::endl;
+
+		static int b = 10;
+		++b;
+		std::cout << b << std::endl;
+	};
+
+	int p = 4;
+
+	std::cout << "p= " << p << std::endl;
+	std::cout << "adr= " << &p << std::endl; // & - показує адресу змінної в пам'яті
+	std::cout << "sizeof= " << sizeof(&p) << std::endl; // sizeof(&змінна) - розмір адреси змінної
+	std::cout << "p- " << sizeof(p) << std::endl; // - розмір змінної
 	return 0;
-}
+}; 
