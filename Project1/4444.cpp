d67a4063ba4844dbabce0f80417ff44e3df0585c
dasha <perowa.d.m@gmail.com>
2025-09-17 19:26:23 +0300

    find min

---
+++ Project1/4444.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a = 5;
+	int b = 7;
+	int min;
+
+	if (a < b) {
+		min = a;
+	}
+	else {
+		min = b;
+	};
+
+	cout << "min = " << min << endl;
+
+
+
+	return 0;
+};
