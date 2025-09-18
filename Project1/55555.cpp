bb449efab7d4e0a9d4c339546df5990f290274b3
dasha <perowa.d.m@gmail.com>
2025-09-18 22:47:07 +0300

    swap

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -3,86 +3,14 @@
 using namespace std;
 
 int main() {
+	int a = 5;
+	int b = 10;
+	cout << a << " " << b << endl;
+	int c = a;
+	a = b;
+	b = c;
+	cout << a << " " << b << endl;
 
-	//МАСИВИ
-	//Масив - сукупність однотипних елементів,
-	//розташованих в пам'яті одним цілим фрагментом.
-
-	/*int mas1[5];
-	double mas2[4] = { 4.5, 5.6, 6.7, 7.8 }; //рахується від нуля
-	int mas3[10] = { 5, 77 };
-	int mas4[] = { 4, 5, 7, 8 };
-
-	double a = mas2[2];
-	cout << a << endl;
-
-	mas2[1] = 100;  //заміна елементу 5.6 на 100
-
-	int mas[5];
-	mas[0] = 5;
-	mas[1] = -6;
-	mas[2] = 4;
-	mas[3] = 3;
-	mas[4] = 1;
-
-	cout << mas[0] << endl;
-	cout << mas[1] << endl;
-	cout << mas[2] << endl;
-	cout << mas[3] << endl;
-	cout << mas[4] << endl;*/
-
-	const int n = 20;
-	int mas[n];
-
-	//unsigned int seed = time(NULL);
-	//srand(seed); //внатуре рандом цифры
-
-	
-
-	for (int i = 0; i < n; ++i) {
-		mas[i] = rand()%21+1;  //випадкове ціле число, % - ділення по модулю
-//число буде від нуля до 99
-//2 - додати ще від'ємні числа (%201 - 100) від -100 до 100
-	}
-
-	
-
-	for (int i = 0; i < n; ++i) {
-		cout << mas[i] << "  ";
-	};
-
-	//mas[i] - поточний елемент масиву
-
-	int imin = 0;
-	int imax = 0;     
-
-	//ми шукаємо мін і макс елемент, 
-	//потім його адресу в масиві і міняємо його
-
-	for (int i = 0; i < n; ++i) {
-		if (mas[i] < mas[imin]) {
-			imin = i;
-		}
-
-		if (mas[i] > mas[imax]) {
-			imax = i;
-		}
-	
-	};
-
-	cout << "\nmin = " << mas[imin] << endl;
-	cout << "\nindex min = " << imin << endl;
-	cout << "\nmax = " << mas[imax] << endl;
-	cout << "\nindex max = " << imax << endl;
-
-	mas[imax] = 1000;
-
-	for (int i = 0; i < n; ++i) {
-		cout << mas[i] << "  ";
-	};
-	//в непарному числі вкінці двійкового запису буде 1 
-	//в парному 0
-
-
+	// ми поміняли місцями значення, використовуючи с як пусту чашку
 	return 0;
 };
