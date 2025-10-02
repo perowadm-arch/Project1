45f62ecab6023da79d116fa7a21c26e77b67105c
dasha <perowa.d.m@gmail.com>
2025-10-02 20:14:49 +0300

    goto error

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -10,15 +10,32 @@ int main(){
 	//return   - повертає результат із функції
 	//continue   - for while do while
 
+	bool err1 = false;
+	bool err2 = true;
+	bool err3 = false;
 
-	int i = 0;
-mitkka1:
-	++i;
-	cout << "nnnn" << endl;
-	if (i<10) goto mitkka1;
+	cout << "processing 1" << endl;
+	if (err1) {
+		goto myend;
+	}
 
-	
+	cout << "processing 2" << endl;
+	if (err2) {
+		goto myend;
+	}
 
+	cout << "processing 3" << endl;
+	if (err3) {
+		goto myend;
+	}
+
+	cout << "processing 4" << endl;
+	goto exit_program;
+
+
+myend:
+	cout << "error" << endl;
+	exit_program:
 	cout << "exit" << endl;
 
 	return 0;
