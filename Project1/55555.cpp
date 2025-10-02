a1fc7b7af1164593627e8b42d3819bd35b262a92
dasha <perowa.d.m@gmail.com>
2025-10-02 21:56:21 +0300

    changing elements of mas with pointer

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -6,7 +6,7 @@ int main() {
 	int mas[5] = { 10, 20, 3, 4, 5 };
 
 	for (int i = 0; i < 5; i++) {
-		cout << mas[i] << " ";
+		cout << mas[i] << " " << endl;
 	}
 
 	cout << mas << endl;  //мас це вже адреса елемента
@@ -18,10 +18,15 @@ int main() {
 	*pmas = 111;
 
 	for (int i = 0; i < 5; i++) {
-		cout << mas[i] << " ";
+		cout << mas[i] << " " << endl;
 	}
 
-	
+	pmas =&mas[3];
+*pmas = 555;
+
+for (int i = 0; i < 5; i++) {
+	cout << mas[i] << " " << endl;
+}
 
 	return 0;
 };
