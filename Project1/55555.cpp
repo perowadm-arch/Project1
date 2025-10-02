1bea8d67c01d6bd9256950256480b4f3739eed83
dasha <perowa.d.m@gmail.com>
2025-10-02 19:12:48 +0300

    break

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main(){
+	//оператори передачі керування
 
+	//break   - switch for while do while
+	//goto    - будь де
+	//return   - повертає результат із функції
+	//continue   - for while do while
+
+	for (int i = 1; i <= 10; i++ ) {
+		
+		if (i % 3 == 0) {
+			break;
+		}
+		cout << i << " ";
+	}
+	cout << "exit" << endl;
 
 	return 0;
 };
