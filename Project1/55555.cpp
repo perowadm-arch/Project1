48dfec5fc4062c1371231853f1d347b08044db7d
dasha <perowa.d.m@gmail.com>
2025-10-15 18:32:13 +0300

    sort positive mas elements

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -5,50 +5,48 @@ using namespace std;
 int main() {
 	 
 	const int n = 10;
-	int mas[n];
-
-	for (int i = 0; i < n; i++) {
-		mas[i] = rand() % 100;
-	}
-
+	int mas[n] = { 55, -42, -11, 556, -433, 1, 5, -7, 3, 11 };
 	int* pmas[n];
 
-	for (int i = 0; i < n; i++) {
-		pmas[i] = mas + i; //&mas[i]
+
+//mas[++index] = 111; те ж саме що 1. ++index; 2. mas[index] = 111;
+//mas[index++] = 111; те ж саме що 1. mas[index] = 111; 2. ++index;
+	//1 - префіксна операція. спочатку збільшення змінної - потім вираз
+	//2 - постфіксна операція. спочатку вираз - потім збільшення змінної
+
+    for (int i = 0; i < n; i++) {
+		cout << mas[i] << "\t";
 	}
 
+	int len = 0;
+
 	for (int i = 0; i < n; i++) {
-		cout << mas[i] << " ";
+		if (mas[i] > 0) {
+			pmas[len++] = &mas[i];
+		}
 	}
 	cout << endl;
 
-	for (int i = 0; i < n; i++) {
-		cout << *pmas[i] << " ";
+	for (int i = 0; i < len; i++) {
+		cout << *pmas[i] << "\t";
 	}
-	cout << endl;
 
-	//bulbashka
-
-	for (int k = 0; k < n-1; k++) {
-		for (int i = 0; i < n-1-k; i++) {
+	for (int k = 0; k < len-1; k++) {
+		for (int i = 0; i < len - 1 - k; i++) {
 			if (*pmas[i + 1] < *pmas[i]) {
-				int* c = pmas[i + 1];
-				pmas[i + 1] = pmas[i];
-				pmas[i] = c;
+				int c = *pmas[i + 1];
+				*pmas[i + 1] = *pmas[i];
+				*pmas[i] = c;
 			}
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		cout << mas[i] << " ";
-	}
-	cout << endl;
+	cout << "\n---------------------------------" << endl;
 
 	for (int i = 0; i < n; i++) {
-		cout << *pmas[i] << " ";
+		cout << mas[i] << "\t";
 	}
 	cout << endl;
-
-
+	
 	return 0;
 };
