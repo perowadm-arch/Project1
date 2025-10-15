ed3019d2d7f480dcf6e7ff7934cd746c23be3850
dasha <perowa.d.m@gmail.com>
2025-10-15 19:46:38 +0300

    beb

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -6,20 +6,20 @@ int main() {
 	//багатовим≥рний масив - матриц€ matr[n] [m]
 	//елемент масиву matr[i] [j]
 
-	const int n = 2;
-	const int m = 3;
+	const int n = 7;
+	const int m = 10;
 	
 	int matr[n][m];
 
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < m; k++) {
-			cin >> matr[i][k];
+			matr[i][k] = rand() % 200 -100;
 		}
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < m; k++) {
-			cout << matr[i][k] <<" ";
+			cout << matr[i][k] <<"\t";
 		}
 		cout << endl;
 	}
