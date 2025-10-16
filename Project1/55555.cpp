78e33f170314632fbc76656c53c7aceaafdc9265
dasha <perowa.d.m@gmail.com>
2025-10-16 21:14:12 +0300

    зміна діагоналей місцями

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -6,7 +6,7 @@ int main() {
 	//квадратна матриц€ - коли стовпчики = р€дки
 	//ознака що елемент на головн≥й д≥агонал≥ - matr[i][i]
 
-	const int n = 7;
+	const int n = 5;
 	int matr[n][n];
 
 	for (int i = 0; i < n; i++) {
@@ -33,20 +33,32 @@ int main() {
 		//	}
 		//} - дуже багато необов'€зкових перев≥рок
 
+		cout << matr[i][n-i-1] << "\t"; //поб≥≥чна д≥агональ
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
 		cout << matr[i][i] << "\t";
 	}
 
-	int* pmin = &matr[0][0];
-	int* pmax = &matr[0][0];
 
-	for (int i = 0; i < n; ++i) {
-		if (matr[i][i] < *pmin) pmin = &matr[i][i];
-		if (matr[i][i] > *pmax) pmax = &matr[i][i];
+	for (int index = 0; index < n; index++) {
+		int c = matr[index][index];
+		matr[index][index] = matr[index][n - 1 - index];
+		matr[index][n - 1 - index] = c;
+	}
+	
+
+	cout << endl << endl;
+
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < n; k++) {
+			cout << matr[i][k] << "\t";
+		}
+		cout << endl;
 	}
 
-	cout << endl << *pmin << "\t" << *pmax << endl;
-	cout << "index min = " << pmin - matr[0] << endl;
-	cout << "index max = " << pmax - matr[0] << endl;
 
 	return 0;
 };
