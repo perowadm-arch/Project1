53358adcabd561fc30fee2bfba083416d00d028b
dasha <perowa.d.m@gmail.com>
2025-10-22 18:18:42 +0300

    обнулення елементів вище обох діагоналей

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -6,7 +6,7 @@ int main() {
 	//квадратна матриця - коли стовпчики = рядки
 	//ознака що елемент на головній діагоналі - matr[i][i]
 
-	const int n = 5;
+	const int n = 7;
 	int matr[n][n];
 
 	for (int i = 0; i < n; i++) {
@@ -23,40 +23,23 @@ int main() {
 		cout << endl;
 	}
 
-	//int c = 0; 
 	
-	for (int i = 0; i < n-1; i++) {
-		for (int k = 0; k < n-1-i; k++) { //вище побічної діагоналі
-			//matr[i][k] = 0;
-		
-			//if (matr[i][k] % 2 != 0) {
-			//	++c;
-			//}
-		}
-	}
-
-
-	for (int i = 1; i < n; i++) {
-		for (int k = n-i; k < n; k++) { //нижче побічної діагоналі
+	for (int i = 0; i < (n-1)/2; i++) {//зупинка на перетині діагоналей
+		for (int k = i+1; k < n-1-i; k++) { //все вище обох діагоналей
 			matr[i][k] = 0;
+		
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < n; k++) { 
+			//matr[i][k] = 0;
 
 		}
 	}
 
-	//cout << endl << c << endl;
-	
-	//int min = 1; //пошук мінімального елемента
-	//for (int i = 1; i < n; i++) {
-	//	for (int k = 0; k < i; k++) { //все під головною діагоналлю
-	//		//matr[i][k] = 1;
-
-	//		if (matr[i][k] < min) {
-	//			min = matr[i][k];
-	//		}
-	//	}
-	//}
-
-	cout << endl;
+	cout << endl << endl;
 
 
 	for (int i = 0; i < n; i++) {
@@ -65,8 +48,6 @@ int main() {
 		}
 		cout << endl;
 	}
-
-	//cout << endl << min;
 
 
 	return 0;
