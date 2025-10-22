94d235c008dc8077b851261f1dcd6d069d91cb6d
dasha <perowa.d.m@gmail.com>
2025-10-22 16:35:29 +0300

    падінг

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -11,7 +11,7 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < n; k++) {
-			matr[i][k] = rand() % 200 - 100;
+			matr[i][k] = rand() % 200 - 100+1;
 		}
 	}
 
@@ -23,39 +23,15 @@ int main() {
 		cout << endl;
 	}
 
-	cout << endl << endl;
-
-
-	for (int i = 0; i < n; i++) {
-		//for (int k = 0; k < n; k++) {
-		//	if (i == k) {
-		//		cout << matr[i][k] << "\t";
-		//	}
-		//} - дуже багато необов'язкових перевірок
-
-		cout << matr[i][n-i-1] << "\t"; //побіічна діагональ
-	}
-
-	cout << endl;
-
-	for (int i = 0; i < n; i++) {
-		cout << matr[i][i] << "\t";
-	}
-
-
-	for (int k = 0; k < n - 1; ++k) //повторення порівнання усіх сусідів
-		for (int i = 0; i < n - 1 - k; ++i)//процедура порівняння усіх сусідів
-		{
-			if (abs(matr[i + 1][i + 1]) < abs(matr[i][i]))
-			{//перестановка елементів за правилом трьох стаканів
-				int c = matr[i + 1][i + 1];
-				matr[i + 1][i + 1] = matr[i][i];
-				matr[i][i] = c;
-			}
+	for (int i = 1; i < n-1; i++) {
+		for (int k = 1; k < n-1; k++) {
+			matr[i][k] = 0;
 		}
+	}
 	
 
 	cout << endl << endl;
+
 
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < n; k++) {
