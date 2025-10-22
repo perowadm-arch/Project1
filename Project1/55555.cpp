76fcc922895ba0832bdda969b15f1b556e085d6b
dasha <perowa.d.m@gmail.com>
2025-10-22 16:40:58 +0300

    0 above the main diagonal

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -23,8 +23,8 @@ int main() {
 		cout << endl;
 	}
 
-	for (int i = 1; i < n-1; i++) {
-		for (int k = 1; k < n-1; k++) {
+	for (int i = 0; i < n-1; i++) {
+		for (int k = i+1; k < n; k++) { //все вище головної діагоналі
 			matr[i][k] = 0;
 		}
 	}
