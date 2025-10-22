8e9bade7f20e60ee440bf888b3b5aa5cda799d7a
dasha <perowa.d.m@gmail.com>
2025-10-22 18:30:51 +0300

    базовий масик

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -2,48 +2,20 @@
 using namespace std;
 
 int main() {
-	 
-	//квадратна матриця - коли стовпчики = рядки
-	//ознака що елемент на головній діагоналі - matr[i][i]
 
-	const int n = 7;
-	int matr[n][n];
+	const int n = 4;
+	const int m = 5;
+	int matr[n][m];
 
 	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < n; k++) {
-			matr[i][k] = rand() % 200 - 100+1;
+		for (int k = 0; k < m; k++) {
+			matr[i][k] = rand() % 200 - 100 + 1;
 		}
 	}
 
 
 	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < n; k++) {
-			cout << matr[i][k] << "\t";
-		}
-		cout << endl;
-	}
-
-	
-	for (int i = 0; i < (n-1)/2; i++) {//зупинка на перетині діагоналей
-		for (int k = i+1; k < n-1-i; k++) { //все вище обох діагоналей
-			matr[i][k] = 0;
-		
-		}
-	}
-
-
-	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < n; k++) { 
-			//matr[i][k] = 0;
-
-		}
-	}
-
-	cout << endl << endl;
-
-
-	for (int i = 0; i < n; i++) {
-		for (int k = 0; k < n; k++) {
+		for (int k = 0; k < m; k++) {
 			cout << matr[i][k] << "\t";
 		}
 		cout << endl;
@@ -51,4 +23,4 @@ int main() {
 
 
 	return 0;
-}
+};
