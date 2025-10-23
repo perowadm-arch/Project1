6650c290075f526b766ec6cb8d0625a025aaaa9b
dasha <perowa.d.m@gmail.com>
2025-10-23 19:27:59 +0300

    формула еквівалентності

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -21,6 +21,15 @@ int main() {
 		cout << endl;
 	}
 
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << matr[i][0] << " " << &matr[i][0] << " " << matr[i] << " " << *matr[i] << endl;
+	}//адреси кожного елементу стовпц€
+
+	cout << endl << matr[1] + 2 << " " << *(matr[1] + 2) << " " << matr[1][2] << " " << & matr[1][2] << endl;
+
+	// matr[i][k] <==> *(matr[i] + k) <==> *(*(matr + i) + k) - формула екв≥валентност≥
 
 	return 0;
 };
