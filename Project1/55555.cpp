a7d5c7100951fcf3a86e62ddef01d95c887871ea
dasha <perowa.d.m@gmail.com>
2025-10-29 22:04:04 +0200

    ANSCII

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -8,5 +8,16 @@ int main() {
 	printf("%c\n", ch);
 	printf("%d\n", ch);
 
+	cout << ch << endl;
+	cout << (int)ch << endl;
+
+	cout << endl;
+
+	//друкування повної таблиці ANSCII
+	for (int c = 0; c <= 255; ++c){
+			unsigned char ch = c;
+			printf("Code: %d\t Value: %c\n", ch, ch);
+	}
+
 	return 0;
 };
