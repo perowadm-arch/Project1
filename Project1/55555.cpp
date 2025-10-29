255ba2e13507e1dc0ba8dce47fc4bd21b0141a60
dasha <perowa.d.m@gmail.com>
2025-10-29 19:07:15 +0200

    створення динамічного масиву показчиків для зберігання двохвимірної матриці

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -4,57 +4,32 @@ using namespace std;
 
 int main() {
 
-	// void* malloc(size_t size) виділення динамічної пам'яті
-	// size_t - unsigned int; size - к-ість пам'яті в байтах;
-	 
-	// void* calloc(size_t num, size_t size) - виділ. динамічної пам'яті
-	// num - к-ість блоків; size - розмір блоку у байтах;
-	 
-	// void* realloc(void* ptr, size_t size) - перевиділення динам пам'яті
-	// ptr - показчик на попередню виділену пам'ять; size - розмір блоку в байтах
-	 
-	// unsigned int - число без знаків (тобто без -)
-
-	// void free(void* ptr); ptr - показчик, який зберігає адресу виділеної динам пам'яті
-
-	//ТИП* prt1 = new ТИП;
-	//ТИП* prt2 = new ТИП(значення);
-	//ТИП* mas = new ТИП[кількість елементів];
-	//виділення динамічної пам’яті
-
-	//delete ptr1;
-	//delete[]mas;
-	//звільнення динамічної пам’яті
-
-	//ТИП* ІМЯ = (ТИП*)malloc(sizeof(ТИП)*n);
-	//free(ІМЯ);
-
-
 	int n;
-	cout << "enter the number of rows: ";
-	cin >> n;
+	int m;
+	do {
+		cout << "enter n>0: " << endl;
+		cin >> n;
 
-//	int* mas = (int*)malloc(sizeof(int)*n); //створення динамічного масиву
 	
-	double* mas = new double[n];
+		cout << "enter m>0: " << endl;
+		cin >> m;
 
-	for (int i = 0; i < n; i++) {
-		mas[i] = rand() % 10000 / 100.0;
-	}
-	
-	for (int i = 0; i < n; i++) {
-		cout << mas[i] << " ";
-	}
+	} while (n <= 0 || m <= 0);
 
-	//free(mas);
-	delete[]mas;
-	
+		if (n <= 0 || m <= 0) {
+			cout << "invalid input" << endl;
+			return 1;
+		}
 
+		double** matr;
+		matr = new double* [n];
+		//int** matr = new int* [n];
 
-
-
-
-
+		for (int i = 0; i < n; i++) {
+			matr[i] = new double[m];
+		}
+		
+	//створення динамічного масиву показчиків для зберігання двохвимірної матриці
 
 
 	return 0;
