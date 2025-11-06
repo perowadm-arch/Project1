7c16151e180ac1f439b7763b790dff6b2e6ea348
dasha <perowa.d.m@gmail.com>
2025-11-06 11:45:28 +0200

    getline

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -6,17 +6,10 @@ using namespace std;
 int main() {
 
 	char str[100];
-	char str1[100];
-	char str2[100];
+	
+	cin.getline(str, 99); //fgets(str,99,stdin);
 
-	cin >> str;
-	cin >> str1;
-	cin >> str2;
-
-	printf("%s\n\n", str);
-	printf("%s\n\n", str1);
-	printf("%s\n\n", str2);
-
+	cout << str << endl;
 
 	return 0;
 };
