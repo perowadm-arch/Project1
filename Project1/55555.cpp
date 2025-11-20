2153b519cf5c118e7a4582ffc62fa330f53a8704
dasha <perowa.d.m@gmail.com>
2025-11-20 12:00:20 +0200

    struct

--- Project1/55555.cpp
+++ Project1/55555.cpp
@@ -1,15 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
+
 using namespace std;
 
 int main() {
 
-	char str[] = "555Michael, don't 23434 leave me here!";
-	
-	int a = atoi(str);
-	cout << a << endl;
+	struct Tpoint2 {
+		int x;
+		int y;
+	}p1,p2 = {10, 30};
+
+	struct Tpoint {
+		char pib[30];
+		int bal;
+	}ivan = { "Ivanov", 90};
 
 	return 0;
 };
